d3415cc70bead23d516ae3e7e48c9d1d81af4ddf
ItzLoghotXD <loghot.gamerz.official@gmail.com>
2026-05-24 21:21:55 +0530

    initial commit

---
+++ src/main/java/me/itzloghotxd/enderchest/EnderChestPlugin.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2026 ItzLoghotXD
+ *
+ * This file is part of "EnderChest" Plugin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, version 3 of the License.
+ */
+
+package me.itzloghotxd.enderchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnderChestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
